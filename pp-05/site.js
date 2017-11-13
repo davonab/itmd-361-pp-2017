b372ad2ef986a62ca4ca51ea95f7eb5610a4d386
davonab <davon.branch42@yahoo.com>
2017-11-12 18:51:46 -0600

    completed javascript problems for pp-05

--- pp-05/site.js
+++ pp-05/site.js
@@ -1,4 +1,18 @@
-function ringDoorbell() {
+$(document).ready(function() {
   var doorbell = new Audio('media/doorbell.mp3');
-  doorbell.play();
-}
+  //if a user has JavaScript, then delete the audio
+  $("#dbell").remove();
+  // when the nav id is pressed, the doorbell will ring
+  $('#doorbell-ring').on('click', function(){
+    ringDoorbell();
+  });
+  // ring doorbell when the key "d" is pressed
+  $(document).on("keypress", function(press){
+    if (press.key === "d") {
+      ringDoorbell();
+    }
+  });
+  function ringDoorbell() {
+    doorbell.play();
+  }
+});
